f89aa3ab76bf90d4c352b7f0884b85ac9d23e636
Muna Dahir <muna.dahir@hotmail.com>
2022-01-10 20:59:00 -0500

    add error messages

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,25 +8,30 @@ RSpec.describe Product, type: :model do
     
     it 'should be valid with all feilds' do
       expect(new_product).to be_valid
+      expect(subject.errors.full_messages).to eql([])
     end
 
     it 'should not be valid without name' do
       new_product.name = nil
       expect(new_product).not_to be_valid
+      expect(subject.errors.full_messages).to eql(["Name can't be blank"])
     end
     it 'should not be valid without price' do
       new_product.price_cents = nil
       expect(new_product).not_to be_valid
+      expect(subject.errors.full_messages).to eql(["Price cents is not a number", "Price is not a number", "Price can't be blank"])
     end
 
     it 'should not be valid without quantity' do
       new_product.quantity = nil
       expect(new_product).not_to be_valid
+      expect(subject.errors.full_messages).to eql(["Quantity can't be blank"])
     end
 
     it 'should not be valid without category' do
       new_product.category = nil
       expect(new_product).not_to be_valid
+      expect(subject.errors.full_messages).to eql(["Category can't be blank"])
     end
     
   end
