efcb52992792721e97024037b3138c4f419b030a
Muna Dahir <muna.dahir@hotmail.com>
2022-01-11 16:31:13 -0500

    fix login for finding user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,17 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
 
-  def self.authenticate_with_credentials (email, password)
-    user = User.find_by_email(email.downcase.strip)
-    if user && user.authenticate(password)
-      user
+  def self.authenticate_with_credentials(email, password)
+    
+    @user = User.find_by_email(email.downcase.strip)
+    if @user && @user.authenticate(password)
+      @user
     else
      nil
     end
   end
+  
 
+  
 
 end
