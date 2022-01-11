2b07ee2b128d7d4e9dd3af6d2561d5ee23f810b7
Muna Dahir <muna.dahir@hotmail.com>
2022-01-11 16:37:03 -0500

    fix form locator

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -5,18 +5,24 @@ RSpec.feature "UserLogins", type: :feature do
 
    # SETUP
    before :each do
-    @category = Category.create! name: 'Apparel'
-
-    10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
+    @user = User.create!( name: 'Sample', email: 'sample@mail.com', password: 'password', password_confirmation: 'password')
   end
 
+  scenario "They see all products" do
+    # ACT
+    visit "/login"
+
+    within('form') do 
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+      click_button 'Submit'
+    end
+
+    # DEBUG 
+    #save_screenshot
+
+    # VERIFY
+    expect(page).to have_text 'Logout'
+  end
   
 end
