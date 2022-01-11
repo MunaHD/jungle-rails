202139aa4a73bf224139edfaafd057baece77ab6
Muna Dahir <muna.dahir@hotmail.com>
2022-01-11 16:08:02 -0500

    fix user authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if User.authenticate_with_credentials(params[:email], params[:password])
+    user = User.authenticate_with_credentials(params[:email], params[:password])
+    if user
       # Save the user id inside the browser cookie. This is how we keep the user 
       # logged in when they navigate around our website.
       session[:user_id] = user.id
