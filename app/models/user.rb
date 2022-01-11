cc04b746e34a62c04c4f026dd6250bf2ca60e281
Muna Dahir <muna.dahir@hotmail.com>
2022-01-10 21:01:03 -0500

    add validation for email and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, presence: true, uniqueness: { case_sensitive: => false}
+  validates :password, presence: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
 
 
