7774c899727aa5ced277db0ad20a5791c7b0d975
Muna Dahir <muna.dahir@hotmail.com>
2022-01-10 19:05:18 -0500

    add test for product with all feilds

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Product, type: :model do
     # can also use subject without a name (in tests put -> subject)and  the name of the calss can be described_class
     # Category.new works with nil feilds
     subject(:new_product) { Product.new(name: "Sample", price_cents: 45, quantity: 60, category: Category.new)}
+    
+    it 'should be valid with all feilds' do
+      expect(new_product).to be_valid
+    end
 
     
   end
