72839247355adaa03ea7a9c43eefdbec4b2864d0
Muna Dahir <muna.dahir@hotmail.com>
2022-01-12 00:07:25 -0500

    fix review table fields

--- db/migrate/20220112045853_create_reviews.rb
+++ db/migrate/20220112045853_create_reviews.rb
@@ -1,10 +1,10 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.string :index
-      t.string :show
-      t.string :create
-      t.string :destroy
+      t.integer :product_id
+      t.integer :user_id
+      t.text :description
+      t.integer :rating
 
       t.timestamps null: false
     end
